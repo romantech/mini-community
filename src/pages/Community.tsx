fc4e7b602ae2fc2278c25483b9f6735ba508be03
Johan <johan@romantech.net>
2022-05-01 16:44:01 +0900

    :adhesive_bandage: 글쓰기 페이지에서 글 목록 불러오는 문제 수정
    - Community.tsx 라우트 조건에 따른 fetch 코드 수정

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,12 +1,6 @@
-/* eslint-disable @typescript-eslint/naming-convention,react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import {
-  Outlet,
-  useLocation,
-  useMatch,
-  useNavigate,
-  useParams,
-} from 'react-router-dom';
+/* eslint-disable @typescript-eslint/naming-convention */
+import React, { useCallback, useEffect } from 'react';
+import { Outlet, useMatch, useNavigate, useParams } from 'react-router-dom';
 import siteUrl from 'routes/url';
 import {
   getCategories,
@@ -21,30 +15,44 @@ import { selectLoading } from 'modules/community/communitySelector';
 export default function Community() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { key } = useLocation(); // location.key : 라우트가 변경될 때마다 생성되는 고유한 식별 키
+
+  const { post_id } = useParams(); // 포스트 상세보기로 진입했을 때
 
   const rootMatch = useMatch(siteUrl.community.root);
-  const { post_id } = useParams(); // 포스트 상세보기로 진입했을 때
+  const listMatch = useMatch(siteUrl.community.list);
+  const newPostMatch = useMatch(siteUrl.community.post.new);
 
   const loading = useSelector(selectLoading);
 
-  useEffect(() => {
-    if (!post_id) {
+  /*
+   * location.key 는 라우트가 변경될 때마다 생성되는 고유한 식별 키 ex) { key } = useLocation()
+   * 라우트가 변경될 때마다 fetch 하고 싶으면 useEffect 종속성 배열에 location.key 를 추가하면 됨
+   * reference: https://stackoverflow.com/a/68848204/3730665
+   * */
+
+  const fetchByRoute = useCallback(() => {
+    if (listMatch) {
       dispatch(getPosts()); // 전체 포스트 목록 GET
       dispatch(getCategories()); // 카테고리 목록 GET
-    } else {
+    } else if (newPostMatch) {
+      dispatch(getCategories());
+    } else if (post_id) {
       dispatch(getPostById({ id: Number(post_id) })); // 선택한 포스트 정보 GET
     }
-  }, [post_id, key]); // location.key 추가해서 라우트 변경시마다 fetch
+  }, [dispatch, listMatch, newPostMatch, post_id]);
+
+  useEffect(() => {
+    fetchByRoute();
+  }, [fetchByRoute]);
 
   useEffect(() => {
     if (rootMatch) navigate(siteUrl.community.list);
-  }, [rootMatch]);
+  }, [navigate, rootMatch]);
 
   return (
     // 아이폰12 Pro 기준 width
     <div className="max-w-[390px] my-0 mx-auto">
-      <Loading loading={loading} delay={100} />
+      <Loading loading={loading} delay={300} />
       <Outlet />
     </div>
   );
