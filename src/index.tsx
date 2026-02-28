c5b5a40a821028cccf6b9a74c5bbe438d0d1f2af
ColorFilter <johan@romantech.net>
2026-02-28 18:05:35 +0900

    🛠️ Refactor extraReducers using builder API

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import 'styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'modules/store';
 import { Provider } from 'react-redux';
-import { Analytics } from '@vercel/analytics/react';
 import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -25,12 +23,6 @@ root.render(
       >
         <App />
       </BrowserRouter>
-      <Analytics />
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example, reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
